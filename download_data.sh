4c39206d429480e091b71b62f22c76bf8c8627d1
Vova Manannikov <vmanannikov@broadsoft.com>
2019-02-22 17:23:37 +0300

    wip: update .sh script access rights

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://s3.amazonaws.com/video.udacity-data.com/topher/2018/November/5be7eb6f_processed-celeba-small/processed-celeba-small.zip && unzip processed-celeba-small.zip && rm -f processed-celeba-small.zip
+wget https://s3.amazonaws.com/video.udacity-data.com/topher/2018/November/5be7eb6f_processed-celeba-small/processed-celeba-small.zip && unzip -q processed-celeba-small.zip && rm -f processed-celeba-small.zip
